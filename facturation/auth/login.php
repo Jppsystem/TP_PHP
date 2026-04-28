78f52a00183befd8b4e0d6ba2581a56709765ff3
ZENAS-0 <tshishimbizenas@gmail.com>
2026-04-28 22:26:03 +0100

    fichier conf.php et formulaire de connexion (login.php)

---
+++ facturation/auth/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="login-form">
+        <H2> TP FACTURATION</H2>
+            <form action="login.php" method="post">
+                <label for="identifiant">Identifiant:</label>
+                <input type="text" name="identifiant" id="identifiant" required><br>
+                <label for="mot_de_passe">Mot de passe:</label>
+                <input type="password" name="mot_de_passe" id="mot_de_passe" required> <br>
+                <input type="submit" value="Se connecter">
+            </form>
+    
+    </div>
+</body>
+</html>
---
+++ facturation/config/config.php
@@ -0,0 +1,29 @@
+<?php
+// Nom et paramètres globaux
+define('APP_NAME', 'TP Facturation');
+define ('APP_LANG', 'fr');
+date_default_timezone_set('Africa/Kinshasa');
+
+// CHEMINS PRINCIPAUX
+define('BASE_PATH',__DIR__.'/../');
+define('BASE_URL','http://facturation.test');
+
+// CHEMINS DES DOSSIERS
+define('ASSETS_PATH', BASE_URL . 'assets/');
+define('CSS_PATH', ASSETS_PATH .'css/');
+define('JS_PATH', ASSETS_PATH .'js/');
+define('MODULES_PATH', BASE_PATH .'modules/');
+define('INCLUDES_PATH', BASE_PATH .'includes/');
+define('AUTH_PATH', BASE_PATH .'auth/');
+define('RAPPORTS_PATH', BASE_PATH .'rapports/');
+define('DATA_PATH', BASE_PATH .'data/');
+define('PRODUITS_PATH', MODULES_PATH .'produits/');
+define('FACTURATION_PATH', MODULES_PATH .'facturation/');
+define('ADMIN_PATH', MODULES_PATH .'admin/');
+define('CONFIG_PATH', BASE_PATH .'config/');
+// CHEMINS DES FICHIERS
+define('PRODUITS_FILE', DATA_PATH .'produits.json');
+define('UTILISATEURS_FILE', DATA_PATH .'utilisateurs.json');
+define('FACTURES_FILE', DATA_PATH .'factures.json');
+
+?>
