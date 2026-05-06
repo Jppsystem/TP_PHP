b7fafeea44177c4f9e880cda92d032caf3ff29da
Joseph-Mukendi <josephmukendi320@gmail.com>
2026-05-06 15:53:05 +0100

    Modification
    Modification

---
+++ facturation/scanner.php
@@ -0,0 +1,8 @@
+<div id="camera" style="width:400px; height:300px; border:1px solid #ccc;"></div>
+
+<form id="formProduit" method="POST" action="modules/produits.php" style="display:none;">
+    <input type="text" id="code_barre" name="code_barre">
+    <button type="submit">Enregistrer produit</button>
+</form>
+
+<script src="assets/js/barcode.js"></script>
